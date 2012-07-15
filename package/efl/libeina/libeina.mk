72e76ee3f900784298e7011f5e8480fd34e3e166
Paul Cercueil <paul@crapouillou.net>
2012-07-15 17:22:28 +0200

    libeina: Bump to version 1.2.1

--- package/efl/libeina/libeina.mk
+++ package/efl/libeina/libeina.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBEINA_VERSION = 1.1.0
+LIBEINA_VERSION = 1.2.1
 LIBEINA_SOURCE = eina-$(LIBEINA_VERSION).tar.bz2
 LIBEINA_SITE = http://download.enlightenment.org/releases/
 LIBEINA_INSTALL_STAGING = YES
