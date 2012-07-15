0a03d0adb686a16e5168b32a1126d80ed5cb3bd6
Paul Cercueil <paul@crapouillou.net>
2012-07-15 19:32:03 +0200

    python-evas: Added package.

---
+++ package/python-efl/python-efl.mk
@@ -0,0 +1,1 @@
+include package/python-efl/*/*.mk
---
+++ package/python-efl/python-evas/python-evas.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-evas -- Python bindings for Evas
+#
+################################################################################
+
+PYTHON_EVAS_VERSION = HEAD
+PYTHON_EVAS_SITE_METHOD = svn
+PYTHON_EVAS_SITE = http://svn.enlightenment.org/svn/e/trunk/BINDINGS/python/python-evas
+PYTHON_EVAS_DEPENDENCIES = python libevas host-python-cython
+PYTHON_EVAS_AUTORECONF = YES
+PYTHON_EVAS_CONF_ENV = PYTHON_INCLUDES="$(STAGING_DIR)/usr/include/python$(PYTHON_VERSION_MAJOR)"
+
+$(eval $(call AUTOTARGETS))
